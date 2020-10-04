0c99b382de40866a3ba6e73ed769ff89f5fb7212
Andrzej Borucki <borucki.andrzej@gmail.com>
2020-10-04 19:36:02 +0200

    mvn compile

--- src/main/g4/gen.sh
+++ src/main/g4/gen.sh
@@ -1,3 +1,3 @@
-java org.antlr.v4.Tool LexBasic.g4 -package org.antlr.gen -o ../java/org/antlr/gen
-java org.antlr.v4.Tool ANTLRv4Lexer.g4 -package org.antlr.gen -o ../java/org/antlr/gen
-java org.antlr.v4.Tool ANTLRv4Parser.g4 -package org.antlr.gen -o ../java/org/antlr/gen
+java org.antlr.v4.Tool LexBasic.g4 -package org.antlr.parser.antlr4 -o ../java/org/antlr/parser/antlr4
+java org.antlr.v4.Tool ANTLRv4Lexer.g4 -package org.antlr.parser.antlr4 -o ../java/org/antlr/parser/antlr4
+java org.antlr.v4.Tool ANTLRv4Parser.g4 -package org.antlr.parser.antlr4 -o ../java/org/antlr/parser/antlr4
